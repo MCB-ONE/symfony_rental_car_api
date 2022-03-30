281ea5f0d35b9b080327a7bc78c2373c2f2a2728
toni <toni.one.dev@gmail.com>
2022-03-30 15:34:15 +0200

    Servicio de mailer configurado y probado

---
+++ mailer/src/Service/Mailer/MailerService.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Mailer\Service\Mailer;
+
+use Mailer\Templating\TwigTemplates;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mailer\Exception\TransportExceptionInterface;
+use Twig\Environment;
+use Symfony\Component\Mime\Email;
+
+class MailerService 
+{
+    private const TEMPLATE_SUBJECT_MAP = [
+        TwigTemplates::USER_REGISTER => 'Bienvenid@!',
+    ];
+
+    private MailerInterface $mailer;
+    private Environment $engine; 
+    private LoggerInterface $logger;
+    private string $mailerDefaultSender;
+
+    public function __construct(MailerInterface $mailer, Environment $engine, LoggerInterface $logger, string $mailerDefaultSender)
+    {
+        $this->mailer = $mailer;
+        $this->engine = $engine;
+        $this->logger = $logger;
+        $this->mailerDefaultSender = $mailerDefaultSender;
+    }
+
+    /**
+     * @throws \Exception
+     */
+    public function send(string $reciver, string $template, array $payload): void
+    {
+        $email = (new Email())
+            ->from($this->mailerDefaultSender)
+            ->to($reciver)
+            ->subject(self::TEMPLATE_SUBJECT_MAP[$template])
+            ->html($this->engine->render($template, $payload));
+
+            try{
+                $this->mailer->send($email);
+            }catch(TransportExceptionInterface $e){
+                $this->logger->error(\sprintf('Error al envíar email: %s', $e->getMessage()));
+            }
+    }
+
+}
